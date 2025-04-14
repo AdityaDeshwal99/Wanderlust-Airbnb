4eceb2c4237cd8079818078aefd102f70f49a72a
AdityaDeshwal99 <adityadeshwal04@gmail.com>
2025-04-14 11:32:54 +0530

    Add Project New Files

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV != "production") {
-  require("dotenv").config();
+  require("dotenv").config(); // to require env file
 }
 
 const express = require("express");
@@ -10,19 +10,20 @@ const methodOverride = require("method-override");
 const path = require("path");
 // for better level templates
 const ejsMate = require("ejs-mate");
+// to require ExpressError class
 const ExpressError = require("./utils/ExpressError.js");
-const listingRouter = require("./routes/listing.js");
-const reviewRouter = require("./routes/review.js");
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-
-const dbUrl = process.env.ATLASDB_URL;
+const dbUrl = process.env.ATLASDB_URL; // database connected at atlas
 
 main()
   .then(() => {
@@ -34,14 +35,11 @@ main()
 
 // to establish connection with mongodb
 async function main() {
-  // await mongoose.connect(MONGO_URL);
   await mongoose.connect(dbUrl);
 }
- 
 // for ejs
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-
 // to extract id from request
 app.use(express.urlencoded({ extended: true }));
 
@@ -49,6 +47,7 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 
 // to use static files in public folder like css files
+
 app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
@@ -63,16 +62,16 @@ store.on("error", () => {
   console.log("ERROR in MONGO SESSION STORE", err);
 });
 
-// defining the session options
+// for express-session
 const sessionOptions = {
   store,
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: true,
   cookie: {
-    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-    maxAge: 1000 * 60 * 60 * 24 * 7,
-    httpOnly: true,
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000, // here we set time for cookie to expire to 7 days
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true, // we use this for preventing against cross scripting attacks
   },
 };
 
@@ -80,38 +79,39 @@ const sessionOptions = {
 //   res.send("Hi, I am root");
 // });
 
-// to use the session
+// to use session
 app.use(session(sessionOptions));
-// to use the flash
+// to use connect-flash
 app.use(flash());
 
+// implementing passport done after implementing session
+// to use passport we require these two
+// a middleware that initializes passport
 app.use(passport.initialize());
+// to implement session
 app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate())); // authenticate that generates a function that is used in Passport's LocalStrategy
-passport.serializeUser(User.serializeUser()); // serializeUser that generates a function that is used by passport to serialize users into the session
-passport.deserializeUser(User.deserializeUser()); // deserializeUser that generates a function that is used by passport to deserialize users into the session
+passport.use(new LocalStrategy(User.authenticate())); // all users should be authenticated through localStrategy by using authenticate method
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-// Suucess and Error Route
+// middleware for flash messages
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  res.locals.currUser = req.user;
+  res.locals.currUser = req.user; // it will store info of req.user
   next();
 });
 
-// Demo User Route
-// app.get("/demouser", async (req, res) => {
+// creating fake user
+
+// app.get("/demouser", async(req, res) =>{
 //   let fakeUser = new User({
 //     email: "student@gmail.com",
-//     username: "deltaa-student",
+//     username: "delta-student"
 //   });
-//   let registeredUser = await User.register(fakeUser, "helloworld"); // register method register a new user instance in db with a given password(helloworld) and it automatically checks if username is unique
-//   res.send(registeredUser);
-// });
-
-// these get request redirect into the home page..
-// app.get("/",(req,res)=>{
-//   res.redirect("/listings")
+// let registeredUser = await User.register(fakeUser, "helloworld"); // register method register a new user instance in db with a given password(helloworld) and it automatically checks if username is unique
+//     res.send(registeredUser);
 // })
 
 // app.get("/testListing", async (req, res) =>{
@@ -128,26 +128,23 @@ app.use((req, res, next) => {
 
 // });
 
-// pure ki pure listing ko use kar rahe hain and all routes ko bhi .
+// these are called parent routes
+
 app.use("/listings", listingRouter);
-
-// pure ke pure reviews route ko use kar rahe hain
+// for reviews
 app.use("/listings/:id/reviews", reviewRouter);
-
 app.use("/", userRouter);
 
-// if request does not matches any route then we send standard response.
+// if request does not matches any route and we want to send standard response
 app.all("*", (req, res, next) => {
-  next(new ExpressError(404, "Page not found"));
+  next(new ExpressError(404, "Page Not Found!"));
 });
 
-// Error handling middlewares.
+// Error handling middleware
 app.use((err, req, res, next) => {
-  let { statusCode = 500, message = "Something went wrong" } = err;
-  //res.status(statusCode).send(message);
+  let { statusCode = 500, message = "Something went wrong!" } = err;
   res.status(statusCode).render("error.ejs", { message });
 });
-
 app.listen(8080, () => {
   console.log("Server is lisening to the port 8080");
 });
